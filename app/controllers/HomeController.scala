d039eb103afccaf5542317e6dc24afb8d4f5b8d6
piotrowskialek <piotrowskialek@icloud.com>
2017-07-09 00:30:05 +0200

    hopefully i know what ive done

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -2,13 +2,19 @@ package controllers
 
 import javax.inject._
 import play.api.mvc._
+import play.api.libs.json._
 
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
   def index = Action {
-    Ok("dupa")
+
+    val json: JsValue = JsObject(Seq(
+      "tak" -> JsString("bylo")
+    ))
+
+    Ok(json)
   }
 
 }
